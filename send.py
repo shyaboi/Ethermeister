6f9b1cce0d248f48507fa24fc7efc5ac0d698782
shyaboi <ian.shane.sears@gmail.com>
2021-06-15 23:49:16 -0700

    sending eth on gnache from api get from stripe success

--- send.py
+++ send.py
@@ -1,40 +1,44 @@
 from web3 import Web3
 import os
-from db import update_bal
+# from db import update_bal
 from dotenv import load_dotenv
 load_dotenv()
-import sqlite3
+# import sqlite3
 
-LINK = os.environ.get("LINK")
-GANACHE = os.environ.get("GANACHE")
+def send_eth(receiver_add):
+    # LINK = os.environ.get("LINK")
+    GANACHE = os.environ.get("GANACHE")
+    TESTSPUBKEY = os.environ.get("TESTSPUBKEY")
+    TESTSPRIVKEY = os.environ.get("TESTSPRIVKEY")
+    TESTRPUBKEY = os.environ.get("TESTRPUBKEY")
 
-web3 = Web3(Web3.HTTPProvider(GANACHE))
-
-
-print(web3.isConnected())
-
-sender_add = 'sender_add'
-sender_pk = 'sender_pk'
-
-receiver_add = 'receiver_add'
-receiver_pk = 'receiver_pk'
+    web3 = Web3(Web3.HTTPProvider(GANACHE))
 
 
-nonce = web3.eth.getTransactionCount(sender_add)
+    print(web3.isConnected())
 
-tx = {
-    'nonce':nonce,
-    'to': receiver_add,
-    'value': web3.toWei(5.22222222222225, 'ether'),
-    'gas':200000,
-    'gasPrice': web3.toWei('1', 'wei')
-}
+    sender_add = TESTSPUBKEY
+    sender_pk = TESTSPRIVKEY
+
+    receiver_add = receiver_add
+    receiver_pk = 'receiver_pk'
 
 
+    nonce = web3.eth.getTransactionCount(sender_add)
+
+    tx = {
+        'nonce':nonce,
+        'to': receiver_add,
+        'value': web3.toWei(5.22, 'ether'),
+        'gas':200000,
+        'gasPrice': web3.toWei('1', 'wei')
+    }
 
 
-signed_tx = web3.eth.account.signTransaction(tx, sender_pk)
-tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-print(web3.toHex(tx_hash))
 
-update_bal(receiver_add)
+
+    signed_tx = web3.eth.account.signTransaction(tx, sender_pk)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print(web3.toHex(tx_hash))
+
+    # update_bal(receiver_add)
